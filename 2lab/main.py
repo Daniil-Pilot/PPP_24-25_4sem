ce9a8d39457a93a9fa5599a8812350366ee8002e
Daniil-Pilot <dgrin0414@gmail.com>
2025-04-10 19:03:33 +0300

    Update main.py

--- 2lab/main.py
+++ 2lab/main.py
@@ -1,8 +1,10 @@
+import uvicorn
 
 def main():
-    # Ваш код здесь
-    pass
+    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
 
 if __name__ == "__main__":
     main()
 
+
+#### http://127.0.0.1:8000/docs
